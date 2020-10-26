73381a133dbe6300629b4d9f5fb911ab69b15917
Guilherme Passarinho <guilhermepassarinho@gmail.com>
2020-10-26 00:43:56 -0300

    started the app

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
---
+++ mobile/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Routes from './routes';
+
+// import { Container } from './styles';
+
+const src = () => {
+  return <Routes />;
+};
+
+export default src;
